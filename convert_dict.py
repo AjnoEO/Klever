7af957e7bd7715e3f80d321be3199b13254e23c4
Ajno <boyko.artiom@gmail.com>
2024-04-28 01:22:39 +0300

    Связал main.py и convert_dict.py

--- convert_dict.py
+++ convert_dict.py
@@ -1,27 +1,28 @@
 import json
 import re
 
-dict_words = {}
-list_of_dictionaries = ["dictionaries/sa-ru-antonova.dsl", "dictionaries/sa-ru-kert.dsl", "dictionaries/sa-ru-kuruch.dsl"]
-for dictionary in list_of_dictionaries:
-    with open(dictionary, "r", encoding='utf-16') as f:
-        source_data = dictionary
-        header = None
-        contents = ""
-        for line in f:
-            if line[0] not in '\t #':
-                if header:
-                    dict_data = {
-                        "contents": contents,
-                        "source": dictionary
-                    }
-                    dict_words[header].append(dict_data)
-                    contents = ''
-                header = (line.replace('\n', '')).lower()
-                header = re.sub(r' \d+$', '', header)
-                header = re.sub(r' [ivx]+$', '', header)
-                dict_words.setdefault(header, [])
-            elif line[0] in '\t ':
-                contents += line
-with open ("klever_dict.json", "w", encoding='utf-8') as f:
-    json.dump(dict_words, f, indent = '\t', ensure_ascii=False)
+def convert(list_of_dictionaries: list[dict]) -> None:
+	"""Конвертирует словари в формат словаря Klever и сохраняет в klever_dict.json"""
+	dict_words = {}
+	for dictionary in list_of_dictionaries:
+		with open(dictionary, "r", encoding='utf-16') as f:
+			source_data = dictionary
+			header = None
+			contents = ""
+			for line in f:
+				if line[0] not in '\t #':
+					if header:
+						dict_data = {
+							"contents": contents,
+							"source": dictionary
+						}
+						dict_words[header].append(dict_data)
+						contents = ''
+					header = (line.replace('\n', '')).lower()
+					header = re.sub(r' \d+$', '', header)
+					header = re.sub(r' [ivx]+$', '', header)
+					dict_words.setdefault(header, [])
+				elif line[0] in '\t ':
+					contents += line
+	with open ("klever_dict.json", "w", encoding='utf-8') as f:
+		json.dump(dict_words, f, indent = '\t', ensure_ascii=False)
--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import json
+import re
+import convert_dict
 
-with open("converted_dict.json", encoding="utf-8") as f:
+list_of_dictionaries = ["dictionaries/sa-ru-antonova.dsl", "dictionaries/sa-ru-kert.dsl", "dictionaries/sa-ru-kuruch.dsl"]
+convert_dict.convert(list_of_dictionaries)
+with open("klever_dict.json", encoding="utf-8") as f:
 	full_dict = json.load(f)
 
 def format_contents(contents: str) -> str: # форматировать содержание словарной статьи
 	content_lines: list[str] = contents.split("\n")
 	if content_lines[-1] == "": content_lines.pop() 
 	content_lines = list(map(lambda s: s.lstrip(), content_lines))
-	return "\n".join(content_lines)
+	contents = "\n".join(content_lines)
+	contents = re.sub(r"\[.+?\]", "", contents)
+	return contents
 
 def article_display(article: dict[str]) -> str: # отображение словарной статьи с источником
 	assert set(article.keys()) == {"contents", "source"}, ValueError(f"Невалидная словарная статья: {article}")
