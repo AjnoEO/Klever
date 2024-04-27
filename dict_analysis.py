0abe40944cbb14fafc2520a5d20e8f4d5c881224
Ajno <boyko.artiom@gmail.com>
2024-04-27 21:08:56 +0300

    blablabla

--- dict_analysis.py
+++ dict_analysis.py
@@ -15,9 +15,9 @@ for d in dictionaries:
 		for line in f:
 			line = line.rstrip()
 			if line[0] not in "\t #":
-				words.append(line)
-				letters = letters | set(line)
-	result[d] = {"Words": words, "Letters": list(letters)}
+				words.append(line.lower())
+				letters = letters | set(line.lower())
+	result[d] = {"Words": words, "Letters": list(sorted(letters))}
 
 with open("dict_info.json", mode="w", encoding="utf-8") as f:
 	json.dump(result, f, indent=4, ensure_ascii=False)
