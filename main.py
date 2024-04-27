4dac980817cbb8038c34216cfa1f897380944eb0
Ajno <boyko.artiom@gmail.com>
2024-04-28 01:03:25 +0300

    Поиск в словаре converted_dict.json и отображение в консоли

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Hello, World!")
+import json
+
+with open("converted_dict.json", encoding="utf-8") as f:
+	full_dict = json.load(f)
+
+def format_contents(contents: str) -> str: # форматировать содержание словарной статьи
+	content_lines: list[str] = contents.split("\n")
+	if content_lines[-1] == "": content_lines.pop() 
+	content_lines = list(map(lambda s: s.lstrip(), content_lines))
+	return "\n".join(content_lines)
+
+def article_display(article: dict[str]) -> str: # отображение словарной статьи с источником
+	assert set(article.keys()) == {"contents", "source"}, ValueError(f"Невалидная словарная статья: {article}")
+	contents, source = article["contents"], article["source"]
+	return format_contents(contents) + "\n    ~ " + source
+
+text = input()
+if text in full_dict:
+	for article in full_dict[text]:
+		print(article_display(article))
+else:
+	print(f"Нет результатов для слова «{text}»")
