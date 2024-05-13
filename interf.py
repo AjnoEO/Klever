746538fe6b68544680a7f3fc6b22e019ef75b05e
Mrtos-s-jerom-na-kontse <tsyrkov.porfiry@yandex.ru>
2024-05-13 13:26:06 +0300

    заработало!

--- interf.py
+++ interf.py
@@ -1,26 +1,32 @@
 import customtkinter
 import customtkinter_ext as cus_ext
 from mix_ups_parser import possible_words_from_input_word
+from utils import apply_arguments
+from convert_dict import convert
 
-class impossible_words(customtkinter.CTkFrame):
-    def __init__(self, master, title="Klever", values=[]):
+class Possible_words(customtkinter.CTkFrame):
+    def __init__(self, master):
         super().__init__(master)
-        self.grid_columnconfigure((0), weight=1)
-        self.grid_rowconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10), weight=1)
-        self.values = values
-        self.title = title
-        self.variants = []
+        self.variants: list[customtkinter.CTkButton] = []
 
         self.title = customtkinter.CTkLabel(self, text="возможные варианты:")
         self.title.grid(row=0, column=0, padx=10, pady=10, sticky="nesw")
 
-    ##def populate(self):
-        self.values = App.button_search_event(self)
-        for i, value in enumerate(self.values):
-            self.word = customtkinter.CTkButton(self, text=value)
-            self.word.grid(row=i + 1, column=0, padx=10, pady=10, sticky="nesw")
-            self.variants.append(self.word)
+    def populate(self, possible_word_list: list[str]):
+        for word_button in self.variants:
+            word_button.destroy()
+        self.variants.clear()
+        for i, word in enumerate(possible_word_list):
+            word_button = customtkinter.CTkButton(self, text=word, command=apply_arguments(self.article, word))
+            word_button.grid(row=i+1, column=0, padx=10, pady=10, sticky="nesw")
+            self.grid_rowconfigure((i+1), weight=0)
+            self.variants.append(word_button)
             print("самсинг из хеппенинг")
+
+    def article(self, word: str):
+        textbox_contents = [cus_ext.FormattedString(word)]
+        self.master.textbox.force_edit(textbox_contents)
+        print(word)
 
 
 class App(customtkinter.CTk):
@@ -33,38 +39,32 @@ class App(customtkinter.CTk):
         ## параметры окна
         self.title("Klever")
         self.geometry("700x500")
-        self.grid_columnconfigure((0, 1, 2), weight=1)
-        self.grid_rowconfigure((0, 1, 2), weight=1)
+        self.grid_columnconfigure((1), weight=1)
+        self.grid_rowconfigure((1), weight=1)
         self.after(0, lambda: self.state('zoomed'))
 
         ## варианты
-        self.possible_words = impossible_words(self)
-        self.possible_words.grid(row=1, column=0, sticky="nesw", rowspan=2, padx=10, pady=10)
+        self.possible_words = Possible_words(self)
+        self.possible_words.grid(row=0, column=0, sticky="nesw", rowspan=2, padx=10, pady=10)
 
         ## строка поиска
         self.text_var = customtkinter.StringVar(value="")
 
         self.entry = customtkinter.CTkEntry(self, placeholder_text="что поищем в этот раз?")
-        self.entry.grid(row=1, column=1, sticky="nsew", padx=10, pady=10)
+        self.entry.grid(row=0, column=1, sticky="nsew", padx=10, pady=10)
 
         ## поисковые параметры
         self.button_search = customtkinter.CTkButton(self, text="искать!", command=self.button_search_event)
-        self.button_search.grid(row=1, column=2, sticky="nsew", padx=10, pady=10)
+        self.button_search.grid(row=0, column=2, sticky="nsew", padx=10, pady=10)
 
         ##вывод
         self.textbox = cus_ext.CTkPrettyTextbox(self)
-        self.textbox.grid(row=2, column=1, columnspan=11, rowspan=2, sticky="nesw", padx=10, pady=10)
-        ##self.textbox.configure(text = self.entry.get())
+        self.textbox.grid(row=1, column=1, columnspan=11, rowspan=2, sticky="nesw", padx=10, pady=10)
 
     def button_search_event(self):
         self.res = possible_words_from_input_word(self.entry.get())
-        print(self.res)
-        return self.res
-        ##textbox_contents = [cus_ext.FormattedString(str(possible_words_from_input_word(self.entry.get())))]
-        ##self.textbox.insert("0.0", self.textbox.force_edit(textbox_contents))
-        ##textbox_contents = [cus_ext.FormattedString(self.entry.get())]
-        ##self.textbox.configure(text=textbox_contents)
-
+        self.possible_words.populate(self.res)
+        self.possible_words.article(self.entry.get())
 
 app = App()
 app.mainloop()
