06d148350b0f35a1b05bfcdaf2a59c47bf504df8
Ajno <boyko.artiom@gmail.com>
2024-05-14 20:49:44 +0300

    Подвёл файлы под pep-8

--- customtkinter_ext.py
+++ customtkinter_ext.py
@@ -1,143 +1,160 @@
+"""Расширение customtkinter под наши задачи"""
+
 import customtkinter
 from customtkinter_formatting_manager import Formatting, FormattedString
 from markup_parser import parse_dsl
 
 
 class InlineLinker:
-	"""
-	Настройка действий по нажатию на ссылки в CTkTextbox или CTkPrettyTextbox
-	"""
-	def __init__(self,
-				 master,
-				 text_color: str | tuple[str, str] = "dark blue",
-				 formatting: Formatting = Formatting.UNDERLINE):
-		self.text: customtkinter.CTkTextbox = master
+    """
+    Настройка действий по нажатию на ссылки в CTkTextbox или CTkPrettyTextbox
+    """
+    def __init__(self,
+                 master,
+                 text_color: str | tuple[str, str] = "dark blue",
+                 formatting: Formatting = Formatting.UNDERLINE):
+        self.text: customtkinter.CTkTextbox = master
 
-		self.text.tag_config(
-			"link", 
-			foreground = text_color, 
-			underline = 1 if Formatting.UNDERLINE in formatting else 0
-		)
-		self.text.tag_bind("link", "<Enter>", self._enter)
-		self.text.tag_bind("link", "<Leave>", self._leave)
-		self.text.tag_bind("link", "<Button-1>", self._click)
+        self.text.tag_config(
+            "link", 
+            foreground = text_color,
+            underline = 1 if Formatting.UNDERLINE in formatting else 0
+        )
+        self.text.tag_bind("link", "<Enter>", self._enter)
+        self.text.tag_bind("link", "<Leave>", self._leave)
+        self.text.tag_bind("link", "<Button-1>", self._click)
 
-		self.click_events = {}
+        self.click_events = {}
 
-	def add(self, on_click):
-		"""
-		Создаёт и имплементирует тэг для ссылки с выполнением функции `on_click` при нажатии
-		"""
-		tag = f"link-{len(self.click_events)}"
-		self.click_events[tag] = on_click
-		return "link", tag
+    def add(self, on_click):
+        """
+        Создаёт и имплементирует тэг для ссылки с выполнением функции `on_click` при нажатии
+        """
+        tag = f"link-{len(self.click_events)}"
+        self.click_events[tag] = on_click
+        return "link", tag
 
-	def _enter(self, event):
-		self.text.configure(cursor="hand2")
+    def _enter(self, _):
+        self.text.configure(cursor="hand2")
 
-	def _leave(self, event):
-		self.text.configure(cursor="xterm")
+    def _leave(self, _):
+        self.text.configure(cursor="xterm")
 
-	def _click(self, event):
-		for tag in self.text.tag_names(customtkinter.CURRENT):
-			if tag.startswith("link-"):
-				self.click_events[tag]()
-				return
+    def _click(self, _):
+        for tag in self.text.tag_names(customtkinter.CURRENT):
+            if tag.startswith("link-"):
+                self.click_events[tag]()
+                return
 
 
 class Formatter:
-	"""
-	Настройка цвета и форматирования текста в CTkTextbox или CTkPrettyTextbox
-	"""
-	forbidden_formatting = [Formatting.BOLD, Formatting.ITALIC]
+    """
+    Настройка цвета и форматирования текста в CTkTextbox или CTkPrettyTextbox
+    """
+    forbidden_formatting = [Formatting.BOLD, Formatting.ITALIC]
 
-	def __init__(self, master):
-		self.text: customtkinter.CTkTextbox = master
+    def __init__(self, master):
+        self.text: customtkinter.CTkTextbox = master
 
-		self.tag_list = ["bold", "italic", "underline", "overstrike"]
-		
-		self.font_family = self.text._font._family
-		self.font_size = self.text._font._size
-		#self.text.tag_config("bold", font=customtkinter.CTkFont(weight='bold'))
-		self.text.tag_config("bold", font=(self.font_family, self.font_size, "bold"))
-		#self.text.tag_config("italic", font=customtkinter.CTkFont(slant='italic'))
-		self.text.tag_config("italic", font=(self.font_family, self.font_size, "italic"))
-		
-		self.text.tag_config("underline", underline=1)
-		self.text.tag_config("overstrike", overstrike=1)
-	
-	def add(self, color: str | None = None, formatting: Formatting | None = None):
-		tags = []
-		if color:
-			tag_name = f"color-{color}"
-			tags.append(tag_name)
-			if tag_name not in self.tag_list:
-				self.text.tag_config(tag_name, foreground=color)
-				self.tag_list.append(tag_name)
-		if formatting:
-			for formatting_option in formatting:
-				#if formatting_option in Formatter.forbidden_formatting:
-				#	raise NotImplementedError(f"{str(formatting_option).capitalize()} formatting is not implemented yet.")
-				tags.append(str(formatting_option))
-		return tuple(tags)
+        self.tag_list = ["bold", "italic", "underline", "overstrike"]
+
+        self.font_family = self.text._font._family
+        self.font_size = self.text._font._size
+        #self.text.tag_config("bold", font=customtkinter.CTkFont(weight='bold'))
+        self.text.tag_config("bold", font=(self.font_family, self.font_size, "bold"))
+        #self.text.tag_config("italic", font=customtkinter.CTkFont(slant='italic'))
+        self.text.tag_config("italic", font=(self.font_family, self.font_size, "italic"))
+
+        self.text.tag_config("underline", underline=1)
+        self.text.tag_config("overstrike", overstrike=1)
+
+    def add(self, color: str | None = None, formatting: Formatting | None = None) -> tuple[str]:
+        """Создать нужные тэги форматирования и вернуть кортеж с ними
+
+        :param color: цвет текста, None по умолчанию
+        :type color: str | None, optional
+        :param formatting: форматирование, None по умолчанию
+        :type formatting: Formatting | None, optional
+        :return: Кортеж тэгов
+        :rtype: tuple[str]
+        """
+        tags = []
+        if color:
+            tag_name = f"color-{color}"
+            tags.append(tag_name)
+            if tag_name not in self.tag_list:
+                self.text.tag_config(tag_name, foreground=color)
+                self.tag_list.append(tag_name)
+        if formatting:
+            for formatting_option in formatting:
+                tags.append(str(formatting_option))
+        return tuple(tags)
 
 
 class CTkPrettyTextbox(customtkinter.CTkTextbox):
-	"""
-	Textbox с более простой настройкой текста и с ссылками с настраиваемыми действиями
-	"""
-	def __init__(self, master: any, **kwargs):
-		super().__init__(master, **kwargs)
-		self.configure(wrap=customtkinter.WORD)
-		self.configure(state=customtkinter.DISABLED)
-	
-	def tag_config(self, tagName, **kwargs):
-		#if "font" in kwargs:
-		#	raise AttributeError("'font' option forbidden, because would be incompatible with scaling")
-		if "font" in kwargs:
-			font = kwargs["font"]
-			if isinstance(font, customtkinter.CTkFont):
-				font = self._apply_font_scaling(font)
-		return self._textbox.tag_config(tagName, **kwargs)
+    """
+    Textbox с более простой настройкой текста и с ссылками с настраиваемыми действиями
+    """
+    def __init__(self, master: any, **kwargs):
+        super().__init__(master, **kwargs)
+        self.configure(wrap=customtkinter.WORD)
+        self.configure(state=customtkinter.DISABLED)
+        self.linker = None
+        self.formatter = None
 
-	def force_edit(self, new_text: list[FormattedString]):
-		"""
-		Заменить весь текст на `new_text`
-		"""
-		self.configure(state = customtkinter.NORMAL)
-		self.delete("0.0", "end")
-		self.linker = InlineLinker(self)
-		self.formatter = Formatter(self)
-		self._pretty_insert("end", new_text)
-		self.configure(state = customtkinter.DISABLED)
+    def tag_config(self, tagName, **kwargs):
+        if "font" in kwargs:
+            font = kwargs["font"]
+            if isinstance(font, customtkinter.CTkFont):
+                font = self._apply_font_scaling(font)
+        return self._textbox.tag_config(tagName, **kwargs)
 
-	def _pretty_insert(self, index: str, text: list[FormattedString], tags: None = None):
-		index = index.lower()
-		for substring in text:
-			self.insert(index, substring.string, (self.linker.add(substring.on_click) if substring.on_click else ()) + self.formatter.add(substring.color, substring.formatting) + (tags if tags else ()))
-			if not index.endswith("end"):
-				index += f" + {len(substring.string)} chars"
+    def force_edit(self, new_text: list[FormattedString]):
+        """
+        Заменить весь текст на `new_text`
+        """
+        self.configure(state = customtkinter.NORMAL)
+        self.delete("0.0", "end")
+        self.linker = InlineLinker(self)
+        self.formatter = Formatter(self)
+        self._pretty_insert("end", new_text)
+        self.configure(state = customtkinter.DISABLED)
+
+    def _pretty_insert(self, index: str, text: list[FormattedString], tags: None = None):
+        index = index.lower()
+        for substring in text:
+            self.insert(index, substring.string,
+                        (self.linker.add(substring.on_click) if substring.on_click else ()) \
+                        + self.formatter.add(substring.color, substring.formatting) \
+                        + (tags if tags else ())
+                        )
+            if not index.endswith("end"):
+                index += f" + {len(substring.string)} chars"
 
 def __main() -> None:
-	app = customtkinter.CTk()
-	app.title("customtkinter_ext — тест")
-	app.geometry("500x500")
-	app.rowconfigure(index=0, weight=1)
-	app.columnconfigure(index=0, weight=1)
+    app = customtkinter.CTk()
+    app.title("customtkinter_ext — тест")
+    app.geometry("500x500")
+    app.rowconfigure(index=0, weight=1)
+    app.columnconfigure(index=0, weight=1)
 
-	pretty_textbox = CTkPrettyTextbox(app)
-	pretty_textbox.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
-	
-	test_string = "\t[m1][b][c]АББРАЙ [/c][/b][i]прил[/i]. дождливый, дождлив; [b]че̄ххч ли аббрай[/b] осень \[дождлива\][/m]\n\t[m0][c red]•[/c][c green]•[/c][c yellow]•[/c][c blue]•[/c][/m]\n\t[m1][b][c]аббрай[/c][/b] см. [b][ref]АББЬР[/ref][/b][/m]\n\t[m0][c red]•[/c][c green]•[/c][c yellow]•[/c][c blue]•[/c][/m]\n"
+    pretty_textbox = CTkPrettyTextbox(app)
+    pretty_textbox.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
 
-	pretty_text = parse_dsl(test_string, on_click=__on_click)
+    test_string = "\t[m1][b][c]АББРАЙ [/c][/b][i]прил[/i]. дождливый, дождлив;" \
+        "[b]че̄ххч ли аббрай[/b] осень \\[дождлива\\][/m]\n\t[m0]" \
+        "[c red]•[/c][c green]•[/c][c yellow]•[/c][c blue]•[/c][/m]\n" \
+        "\t[m1][b][c]аббрай[/c][/b] см. [b][ref]АББЬР[/ref][/b][/m]\n" \
+        "\t[m0][c red]•[/c][c green]•[/c][c yellow]•[/c][c blue]•[/c][/m]\n"
 
-	pretty_textbox.force_edit(pretty_text)
+    pretty_text = parse_dsl(test_string, on_click=__on_click)
 
-	app.mainloop()
+    pretty_textbox.force_edit(pretty_text)
+
+    app.mainloop()
 
 def __on_click(text: str):
-	print(f"Нажата REF-ссылка: {text}")
+    print(f"Нажата REF-ссылка: {text}")
 
 if __name__ == "__main__":
-	__main()
+    __main()
--- markup_parser.py
+++ markup_parser.py
@@ -1,162 +1,194 @@
-from customtkinter_formatting_manager import Formatting, FormattedString
+"""Преобразование markup-а в список форматированных строк"""
+
 from typing import Callable
 import re
 import inspect
+from customtkinter_formatting_manager import Formatting, FormattedString
 from utils import apply_arguments
 
-def __split_dsl_to_tagged_list(string: str, lstrip: bool = True, rstrip: bool = True) -> list[tuple[str, list[tuple[str, str]]]]:
-	"""Разбивает строку с тэгами DSL на список кортежей. Каждый кортеж — строка символов + список тэгов в виде кортежа (тэг, аргументы тэга)
 
-	:param string: Строка в формате словарной статьи в DSL
-	:type string: str
-	:param lstrip: Удалять ли пробельные символы в начале строки, `True` по умолчанию
-	:type lstrip: bool, optional
-	:param rstrip: Удалять ли пробельные символы в конце строки, `True` по умолчанию
-	:type rstrip: bool, optional
-	:return: Список строк символов и тэгов, которые требуется к ним применить
-	:rtype: list[tuple[str, list[tuple[str, str]]]]
-	"""
-	re_expression = r"^((?:.|\n)*?)\[(\w+?)(|[ 0-9].*?)\](.*?)\[/\2\]((?:.|\n)*)$"
-	tag_match = re.match(re_expression, string)
-	if not tag_match:
-		if lstrip:
-			string = string.lstrip()
-		if rstrip:
-			string = string.rstrip()
-		return [(string, [])]
-	prefix, tag, tag_attribute, internal, suffix = tag_match.groups()
-	prefix = prefix.replace("\\", "")
-	internal = internal.replace("\\", "")
-	suffix = suffix.replace("\\", "")
-	if lstrip:
-		prefix = prefix.lstrip()
-	if rstrip:
-		suffix = suffix.rstrip()
-	parsed_prefix = [(prefix, [])]
-	parsed_internal = __split_dsl_to_tagged_list(internal, lstrip=False, rstrip=False)
-	if tag == 'm':
-		s, tags = parsed_internal[0]
-		parsed_internal = [(s, tags + [(tag, tag_attribute)])] + parsed_internal[1:]
-	else:
-		parsed_internal = [(s, tags + [(tag, tag_attribute)]) for s, tags in parsed_internal]
-	if suffix != "" and suffix[0] == "\n":
-		parsed_suffix = [("\n", [])] + __split_dsl_to_tagged_list(suffix)
-	else:
-		parsed_suffix = __split_dsl_to_tagged_list(suffix, lstrip=False)
-	list_of_tagged_strings = (parsed_prefix if prefix != "" else []) \
-		+ parsed_internal \
-		+ (parsed_suffix if suffix != "" else [])
-	return list_of_tagged_strings
+def __split_dsl_to_tagged_list(
+    string: str, lstrip: bool = True, rstrip: bool = True
+) -> list[tuple[str, list[tuple[str, str]]]]:
+    """Разбивает строку с тэгами DSL на список кортежей.
+     Каждый кортеж — строка символов + список тэгов в виде кортежа (тэг, аргументы тэга)
+
+    :param string: Строка в формате словарной статьи в DSL
+    :type string: str
+    :param lstrip: Удалять ли пробельные символы в начале строки, `True` по умолчанию
+    :type lstrip: bool, optional
+    :param rstrip: Удалять ли пробельные символы в конце строки, `True` по умолчанию
+    :type rstrip: bool, optional
+    :return: Список строк символов и тэгов, которые требуется к ним применить
+    :rtype: list[tuple[str, list[tuple[str, str]]]]
+    """
+    tag_match = re.match(
+        r"^((?:.|\n)*?)\[(\w+?)(|[ 0-9].*?)\](.*?)\[/\2\]((?:.|\n)*)$", string
+    )
+    if not tag_match:
+        if lstrip:
+            string = string.lstrip()
+        if rstrip:
+            string = string.rstrip()
+        return [(string, [])]
+    prefix, tag, tag_attribute, internal, suffix = tag_match.groups()
+    prefix = prefix.replace("\\", "")
+    internal = internal.replace("\\", "")
+    suffix = suffix.replace("\\", "")
+    if lstrip:
+        prefix = prefix.lstrip()
+    if rstrip:
+        suffix = suffix.rstrip()
+    parsed_prefix = [(prefix, [])]
+    parsed_internal = __split_dsl_to_tagged_list(internal, lstrip=False, rstrip=False)
+    if tag == "m":
+        s, tags = parsed_internal[0]
+        parsed_internal = [(s, tags + [(tag, tag_attribute)])] + parsed_internal[1:]
+    else:
+        parsed_internal = [
+            (s, tags + [(tag, tag_attribute)]) for s, tags in parsed_internal
+        ]
+    if suffix != "" and suffix[0] == "\n":
+        parsed_suffix = [("\n", [])] + __split_dsl_to_tagged_list(suffix)
+    else:
+        parsed_suffix = __split_dsl_to_tagged_list(suffix, lstrip=False)
+    list_of_tagged_strings = (
+        (parsed_prefix if prefix != "" else [])
+        + parsed_internal
+        + (parsed_suffix if suffix != "" else [])
+    )
+    return list_of_tagged_strings
+
 
 def __adapt_on_click_func(
-		on_click: Callable[[str], None] | Callable[[], None] | None,
-		substring: str,
-		substring_adapter: Callable[[str], str] | None = None
-		) -> Callable[[], None] | None:
-	"""Адаптировать функцию `on_click` для использования в ref-ссылке
+    on_click: Callable[[str], None] | Callable[[], None] | None,
+    substring: str,
+    substring_adapter: Callable[[str], str] | None = None,
+) -> Callable[[], None] | None:
+    """Адаптировать функцию `on_click` для использования в ref-ссылке
 
-	:param on_click: Функция `on_click`
-	:type on_click: Callable[[str], None] | Callable[[], None] | None
-	:param substring: Строка в тэге [ref]
-	:type substring: str
-	:param substring_adapter: Функция адаптирующая строку в тэге для создания правильной ссылки, None по умолчанию
-	:type substring_adapter: Callable[[str], str] | None, optional
-	:return: Функция нужного вида
-	:rtype: Callable[[], None] | None
-	"""
-	if not on_click:
-		return None
-	sig = inspect.signature(on_click)
-	num_of_parameters = len(sig.parameters)
-	if num_of_parameters == 1:
-		if substring_adapter:
-			return apply_arguments(on_click, substring_adapter(substring))
-		else:
-			return apply_arguments(on_click, substring)
-	else:
-		return on_click
+    :param on_click: Функция `on_click`
+    :type on_click: Callable[[str], None] | Callable[[], None] | None
+    :param substring: Строка в тэге [ref]
+    :type substring: str
+    :param substring_adapter: Функция адаптирующая строку в тэге
+     для создания правильной ссылки, None по умолчанию
+    :type substring_adapter: Callable[[str], str] | None, optional
+    :return: Функция нужного вида
+    :rtype: Callable[[], None] | None
+    """
+    if not on_click:
+        return None
+    sig = inspect.signature(on_click)
+    num_of_parameters = len(sig.parameters)
+    if num_of_parameters == 1:
+        if substring_adapter:
+            return apply_arguments(on_click, substring_adapter(substring))
+        else:
+            return apply_arguments(on_click, substring)
+    else:
+        return on_click
+
 
 def __parse_dsl_tag(
-		substring: str,
-		tag: str, 
-		tag_arguments: str, 
-		tag_info: dict[str], 
-		on_click: Callable[[str], None] | Callable[[], None] | None,
-		substring_adapter: Callable[[str], str] | None = None
-		) -> dict[str]:
-	"""Парсит DSL-тэг и возвращает словарь информации с нужными изменениями
+    substring: str,
+    tag: str,
+    tag_arguments: str,
+    tag_info: dict[str],
+    on_click: Callable[[str], None] | Callable[[], None] | None,
+    substring_adapter: Callable[[str], str] | None = None,
+) -> dict[str]:
+    """Парсит DSL-тэг и возвращает словарь информации с нужными изменениями
 
-	:param substring: Строка, заключённая в тэг
-	:type substring: str
-	:param tag: Тэг
-	:type tag: str
-	:param tag_arguments: Аргументы тэга
-	:type tag_arguments: str
-	:param tag_info: Начальный словарь информации
-	:type tag_info: dict[str]
-	:param on_click: Функция, выполняющаяся при нажатии на ref-ссылки
-	:type on_click: Callable[[str], None] | Callable[[], None] | None
-	:param substring_adapter: Функция адаптирующая строку в тэге для создания правильной ссылки, None по умолчанию
-	:type substring_adapter: Callable[[str], str] | None, optional
-	:return: Обновлённый словарь информации
-	:rtype: dict[str]
-	"""
-	match tag:
-		case 'c':
-			if tag_arguments == '':
-				tag_info["color"] = "Green"
-			else:
-				tag_info["color"] = tag_arguments
-		case 'm':
-			if not tag_arguments.isnumeric():
-				raise ValueError(f"Invalid tag: {tag, tag_arguments}")
-			tag_info["tabs"] = int(tag_arguments)
-		case 'u':
-			tag_info["formatting"] |= Formatting.UNDERLINE
-		case 'b':
-			tag_info["formatting"] |= Formatting.BOLD
-		case 'i':
-			tag_info["formatting"] |= Formatting.ITALIC
-		case 'ref':
-			tag_info["on_click"] = __adapt_on_click_func(on_click, substring, substring_adapter)
-		case _:
-			raise ValueError(f"Unknown tag: {tag}")
-	return tag_info
+    :param substring: Строка, заключённая в тэг
+    :type substring: str
+    :param tag: Тэг
+    :type tag: str
+    :param tag_arguments: Аргументы тэга
+    :type tag_arguments: str
+    :param tag_info: Начальный словарь информации
+    :type tag_info: dict[str]
+    :param on_click: Функция, выполняющаяся при нажатии на ref-ссылки
+    :type on_click: Callable[[str], None] | Callable[[], None] | None
+    :param substring_adapter: Функция адаптирующая строку в тэге
+     для создания правильной ссылки, None по умолчанию
+    :type substring_adapter: Callable[[str], str] | None, optional
+    :return: Обновлённый словарь информации
+    :rtype: dict[str]
+    """
+    match tag:
+        case "c":
+            if tag_arguments == "":
+                tag_info["color"] = "Green"
+            else:
+                tag_info["color"] = tag_arguments
+        case "m":
+            if not tag_arguments.isnumeric():
+                raise ValueError(f"Invalid tag: {tag, tag_arguments}")
+            tag_info["tabs"] = int(tag_arguments)
+        case "u":
+            tag_info["formatting"] |= Formatting.UNDERLINE
+        case "b":
+            tag_info["formatting"] |= Formatting.BOLD
+        case "i":
+            tag_info["formatting"] |= Formatting.ITALIC
+        case "ref":
+            tag_info["on_click"] = __adapt_on_click_func(
+                on_click, substring, substring_adapter
+            )
+        case _:
+            raise ValueError(f"Unknown tag: {tag}")
+    return tag_info
 
-def parse_dsl(string: str, on_click: Callable[[str], None] | Callable[[], None] | None = None) -> list[FormattedString]:
-	"""Преобразовать строку формата DSL в `list[FormattedString]` с нужным форматированием
 
-	:param string: Строка в формате словарной статьи в DSL
-	:type string: str
-	:param on_click: Функция, которая вызывается по нажатии на ref-ссылки. Можно назначить функцию с аргументом типа `str` (текст ref-ссылки)
-	:type on_click: Callable[[str], None] | Callable[[], None] | None, optional
-	:return: Форматтированная строка для использования в CTkPrettyTextbox
-	:rtype: list[FormattedString]
-	"""
-	splitted_string = __split_dsl_to_tagged_list(string)
-	list_of_formatted_strings: list[FormattedString] = []
-	for substring, tags in splitted_string:
-		tag_info = {
-			"color": None,
-			"formatting": Formatting.NONE,
-			"on_click": None
-		}
-		for tag, tag_arguments in tags:
-			tag_arguments = tag_arguments.lstrip()
-			tag_info = __parse_dsl_tag(substring, tag, tag_arguments, tag_info, on_click, lambda string: re.sub(r"[ \d,]+^", "", string.lower()))
-		if "tabs" in tag_info:
-			list_of_formatted_strings.append(FormattedString('\t' * tag_info["tabs"]))
-		formatted_substring = FormattedString(
-			substring, 
-			color=tag_info["color"], 
-			formatting=tag_info["formatting"], 
-			on_click=tag_info["on_click"])
-		list_of_formatted_strings.append(formatted_substring)
-	return list_of_formatted_strings
+def parse_dsl(
+    string: str, on_click: Callable[[str], None] | Callable[[], None] | None = None
+) -> list[FormattedString]:
+    """Преобразовать строку формата DSL в `list[FormattedString]` с нужным форматированием
+
+    :param string: Строка в формате словарной статьи в DSL
+    :type string: str
+    :param on_click: Функция, которая вызывается по нажатии на ref-ссылки.
+     Можно назначить функцию с аргументом типа `str` (текст ref-ссылки)
+    :type on_click: Callable[[str], None] | Callable[[], None] | None, optional
+    :return: Форматтированная строка для использования в CTkPrettyTextbox
+    :rtype: list[FormattedString]
+    """
+    splitted_string = __split_dsl_to_tagged_list(string)
+    list_of_formatted_strings: list[FormattedString] = []
+    for substring, tags in splitted_string:
+        tag_info = {"color": None, "formatting": Formatting.NONE, "on_click": None}
+        for tag, tag_arguments in tags:
+            tag_arguments = tag_arguments.lstrip()
+            tag_info = __parse_dsl_tag(
+                substring,
+                tag,
+                tag_arguments,
+                tag_info,
+                on_click,
+                lambda string: re.sub(r"[ \d,]+^", "", string.lower()),
+            )
+        if "tabs" in tag_info:
+            list_of_formatted_strings.append(FormattedString("\t" * tag_info["tabs"]))
+        formatted_substring = FormattedString(
+            substring,
+            color=tag_info["color"],
+            formatting=tag_info["formatting"],
+            on_click=tag_info["on_click"],
+        )
+        list_of_formatted_strings.append(formatted_substring)
+    return list_of_formatted_strings
+
 
 def main():
-	test_string = "\t[m1][b][c]АББРАЙ [/c][/b][i]прил[/i]. дождливый, дождлив; [b][ref]че̄ххч 2[/ref] ли аббрай[/b] осень \[дождлива\][/m]\n\t[m0][c red]•[/c][c green]•[/c][c yellow]•[/c][c blue]•[/c][/m]\n"
-	print(parse_dsl(test_string, on_click=lambda text: print(f"REF-ссылка: <{text}>")))
+    """пеп-8 требует написать что такое main()... дожили"""
+    test_string = (
+        "\t[m1][b][c]АББРАЙ [/c][/b][i]прил[/i]. дождливый, дождлив;"
+        "[b][ref]че̄ххч 2[/ref] ли аббрай[/b] осень \\[дождлива\\][/m]"
+        "\t[m0][c red]•[/c][c green]•[/c][c yellow]•[/c][c blue]•[/c][/m]\n"
+    )
+    print(parse_dsl(test_string, on_click=lambda text: print(f"REF-ссылка: <{text}>")))
+
 
 if __name__ == "__main__":
-	main()
+    main()
