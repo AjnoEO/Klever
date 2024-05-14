c3b01582cab52cbc69a999e6bc44ac7293224376
Ajno <boyko.artiom@gmail.com>
2024-05-14 21:10:36 +0300

    Подвёл файлы под pep-8

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from convert_dict import convert
+"""Основной файл, запускает приложение"""
 
-list_of_dictionaries = ["dictionaries/sa-ru-antonova.dsl", "dictionaries/sa-ru-kert.dsl", "dictionaries/sa-ru-kuruch.dsl"]
-convert(list_of_dictionaries)
-
+import json
 import customtkinter
 import customtkinter_ext as cus_ext
 from mix_ups_parser import possible_words_from_input_word
 from utils import apply_arguments
-import json
 from markup_parser import parse_dsl
 
 with open("klever_dict.json", encoding="utf8") as f:
@@ -16,7 +13,8 @@ with open("klever_dict.json", encoding="utf8") as f:
 with open("info_about_dict.txt", encoding="utf8") as f:
     info_a = f.read()
 
-class Possible_words(customtkinter.CTkFrame):
+
+class PossibleWords(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
         self.variants: list[customtkinter.CTkButton] = []
@@ -29,9 +27,16 @@ class Possible_words(customtkinter.CTkFrame):
             word_button.destroy()
         self.variants.clear()
         for i, word in enumerate(possible_word_list):
-            word_button = customtkinter.CTkButton(self, text=word, fg_color="#E7ECEF", hover_color="#E7ECEF", border_width=0, command=apply_arguments(self.display_article, word))
-            word_button.grid(row=i+1, column=0, padx=10, pady=10, sticky="nesw")
-            self.grid_rowconfigure((i+1), weight=0)
+            word_button = customtkinter.CTkButton(
+                self,
+                text=word,
+                fg_color="#E7ECEF",
+                hover_color="#E7ECEF",
+                border_width=0,
+                command=apply_arguments(self.display_article, word),
+            )
+            word_button.grid(row=i + 1, column=0, padx=10, pady=10, sticky="nesw")
+            self.grid_rowconfigure((i + 1), weight=0)
             self.variants.append(word_button)
 
     def display_article(self, word: str):
@@ -43,7 +48,8 @@ class Possible_words(customtkinter.CTkFrame):
         self.master.textbox.force_edit(textbox_contents)
         ##print(word)
 
-class Win_about(customtkinter.CTkToplevel):
+
+class WinAbout(customtkinter.CTkToplevel):
     def __init__(self, master):
         super().__init__(master)
 
@@ -59,10 +65,12 @@ class Win_about(customtkinter.CTkToplevel):
         self.info_about.grid(row=0, column=0, sticky="nesw", padx=20, pady=20)
         self.info_about.force_edit([cus_ext.FormattedString(info_a)])
 
+
 class App(customtkinter.CTk):
     customtkinter.set_default_color_theme("style.json")
 
     words = []
+
     def __init__(self):
         super().__init__()
 
@@ -71,12 +79,14 @@ class App(customtkinter.CTk):
         self.geometry("700x500")
         self.grid_columnconfigure((1), weight=1)
         self.grid_rowconfigure((1), weight=1)
-        self.after(0, lambda: self.state('zoomed'))
+        self.after(0, lambda: self.state("zoomed"))
         self.minsize(900, 500)
 
         ## варианты
-        self.possible_words = Possible_words(self)
-        self.possible_words.grid(row=0, column=0, sticky="nesw", rowspan=2, padx=(20, 10), pady=20)
+        self.possible_words = PossibleWords(self)
+        self.possible_words.grid(
+            row=0, column=0, sticky="nesw", rowspan=2, padx=(20, 10), pady=20
+        )
 
         ## строка поиска
         self.entry = customtkinter.CTkEntry(self, placeholder_text="введите услышанное")
@@ -84,35 +94,54 @@ class App(customtkinter.CTk):
         self.entry.bind("<KeyPress-Return>", command=self.enter_pressed)
 
         ## кнопка поиска
-        self.button_search = customtkinter.CTkButton(self, text="искать!", command=self.button_search_event)
+        self.button_search = customtkinter.CTkButton(
+            self, text="искать!", command=self.button_search_event
+        )
         self.button_search.grid(row=0, column=2, sticky="nsew", padx=10, pady=(20, 10))
 
         ## кнопка инфо
         self.button_info = customtkinter.CTkButton(self, text="о проекте")
-        self.button_info.grid(row=0, column=3, sticky="nsew", padx=(10, 20), pady=(20, 10))
+        self.button_info.grid(
+            row=0, column=3, sticky="nsew", padx=(10, 20), pady=(20, 10)
+        )
         self.button_info.bind("<ButtonRelease>", command=self.info)
 
         ##вывод
         self.textbox = cus_ext.CTkPrettyTextbox(self)
-        self.textbox.grid(row=1, column=1, columnspan=11, rowspan=2, sticky="nesw", padx=(10, 20), pady=(10, 20))
+        self.textbox.grid(
+            row=1,
+            column=1,
+            columnspan=11,
+            rowspan=2,
+            sticky="nesw",
+            padx=(10, 20),
+            pady=(10, 20),
+        )
 
         self.win_info = None
 
     def button_search_event(self):
-        self.res = possible_words_from_input_word(self.entry.get().lower())
-        self.possible_words.populate(self.res)
-        if self.res != []:
-            self.possible_words.display_article(self.res[0])
+        res = possible_words_from_input_word(self.entry.get().lower())
+        self.possible_words.populate(res)
+        if res:
+            self.possible_words.display_article(res[0])
         else:
-            self.textbox.force_edit([cus_ext.FormattedString("мы очень старались, но ничего похожего не нашлось :(")])
+            self.textbox.force_edit(
+                [
+                    cus_ext.FormattedString(
+                        "мы очень старались, но ничего похожего не нашлось :("
+                    )
+                ]
+            )
 
-    def enter_pressed(self, event):
+    def enter_pressed(self, _):
         self.button_search_event()
 
-    def info(self, event):
+    def info(self, _):
         if self.win_info is None or not self.win_info.winfo_exists():
-            self.win_info = Win_about(self)
+            self.win_info = WinAbout(self)
         self.win_info.after(10, self.win_info.focus)
+
 
 app = App()
 app.mainloop()
--- mix_ups_parser.py
+++ mix_ups_parser.py
@@ -1,40 +1,50 @@
+"""Разбирается в путаницах"""
+
 import json
+from convert_dict import convert
 
 MAX_WEIGHT = 5
 
-with open('klever_dict.json', encoding='utf8') as f:
+list_of_dictionaries = [
+    "dictionaries/sa-ru-antonova.dsl",
+    "dictionaries/sa-ru-kert.dsl",
+    "dictionaries/sa-ru-kuruch.dsl",
+]
+convert(list_of_dictionaries)
+
+with open("klever_dict.json", encoding="utf8") as f:
     dict_words = json.load(f)
     dict_words = dict_words.keys()
 
 # считываю mix_ups/groups.json
 # считываю mix_ups.csv
-mix_ups = [
-    (["э"], ["ы"]),
-    (["н", "ҍ"], ["н", "ь"]),
-    (["д", "т"], ["д"])
-]
+mix_ups = [(["э"], ["ы"]), (["н", "ҍ"], ["н", "ь"]), (["д", "т"], ["д"])]
 
-#для тестировки можно использовать test_dict_keys с посылкой "адынд"
-test_dict_keys = ["адынд", "адындт", "адэнд", "адтэндт", "эдтынд", "адтынд", "адтындт" ]
+# для тестировки можно использовать test_dict_keys с посылкой "адынд"
+test_dict_keys = ["адынд", "адындт", "адэнд", "адтэндт", "эдтынд", "адтынд", "адтындт"]
+
 
 def __results_of_mixup(input_word: str, mix_up: tuple[list, list]) -> list[str]:
     list_of_mistakes = []
     length = len(mix_up[1])
-    for index in range (len(input_word)):
+    for index in range(len(input_word)):
         is_match = True
-        for mix_up_index in range (length):
-            if (index+mix_up_index)>=len(input_word) or input_word[index + mix_up_index] != mix_up[1][mix_up_index]:
+        for mix_up_index in range(length):
+            if (index + mix_up_index) >= len(input_word) or input_word[
+                index + mix_up_index
+            ] != mix_up[1][mix_up_index]:
                 is_match = False
                 break
         if not is_match:
             continue
         prefix = input_word[:index]
-        suffix = input_word[index+length:]
+        suffix = input_word[index + length :]
         new_word = prefix + "".join(mix_up[0]) + suffix
         list_of_mistakes.append(new_word)
     return list_of_mistakes
 
-def apply_mixups(input_word: str, weight: int=0) -> dict[str, int]:
+
+def apply_mixups(input_word: str, weight: int = 0) -> dict[str, int]:
     main_dict = {}
     if input_word in dict_words:
         main_dict[input_word] = weight
@@ -43,15 +53,17 @@ def apply_mixups(input_word: str, weight: int=0) -> dict[str, int]:
     for mix_up in mix_ups:
         list_of_mistakes = __results_of_mixup(input_word, mix_up)
         for mistaken_word in list_of_mistakes:
-            aux_dict = apply_mixups(mistaken_word, weight+1)
+            aux_dict = apply_mixups(mistaken_word, weight + 1)
             main_dict = aux_dict | main_dict
     return main_dict
 
+
 def possible_words_from_input_word(input_word):
     main_dict = apply_mixups(input_word)
-    sorted_main_dict = dict(sorted(main_dict.items(), key = lambda x:(x[1], x[0])))
+    sorted_main_dict = dict(sorted(main_dict.items(), key=lambda x: (x[1], x[0])))
     list_of_possible_words = list(sorted_main_dict.keys())
     return list_of_possible_words
 
+
 if __name__ == "__main__":
-    print (possible_words_from_input_word(input()))
+    print(possible_words_from_input_word(input()))
