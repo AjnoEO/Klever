68ee25c76f20486ef06279bab533acc76aab3cca
Mrtos-s-jerom-na-kontse <tsyrkov.porfiry@yandex.ru>
2024-05-16 16:23:28 +0300

    добавил клавиатуру со сложными буквами!

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import customtkinter_ext as cus_ext
 from mix_ups_parser import possible_words_from_input_word
 from utils import apply_arguments
 from markup_parser import parse_dsl
+import math
 
 with open("klever_dict.json", encoding="utf8") as f:
     dictionaries = json.load(f)
@@ -13,128 +14,86 @@ with open("klever_dict.json", encoding="utf8") as f:
 with open("info_about_dict.txt", encoding="utf8") as f:
     info_a = f.read()
 
-class Letterpad(customtkinter.CTk):
+class Letterpad(customtkinter.CTkFrame):
 
-    def __init__(self):
-        super().__init__()
-
-        ## параметры окна
-        self.title("letterpad")
-        self.geometry("220x170")
-        self.grid_columnconfigure((0, 1, 2, 3), weight=1)
-        self.grid_rowconfigure((0, 1, 2), weight=1)
-        ##self.after(0, lambda: self.state("zoomed"))
-        ##self.minsize(900, 500)
-
-        ## ӓ ӭ һ ј ҋ ӆ ӎ ӊ ӈ ҏ ҍ
-        self.dotted_a = customtkinter.CTkButton(
-            self, text="ӓ", command=self.button_dotted_a
-        )
-        self.dotted_a.grid(row=0, column=0, sticky="nsew", padx=(20, 10), pady=(20, 10))
-
-        self.dotted_e = customtkinter.CTkButton(
-            self, text="ӭ", command=self.button_dotted_e
-        )
-        self.dotted_e.grid(row=0, column=1, sticky="nsew", padx=10, pady=(20, 10))
-
-        self.h = customtkinter.CTkButton(
-            self, text="һ", command=self.button_h
-        )
-        self.h.grid(row=0, column=2, sticky="nsew", padx=10, pady=(20, 10))
-
-        self.j = customtkinter.CTkButton(
-            self, text="ј", command=self.button_j
-        )
-        self.j.grid(row=0, column=3, sticky="nsew", padx=(10, 20), pady=(20, 10))
-
-        self.cyrillic_j = customtkinter.CTkButton(
-            self, text="ҋ", command=self.button_cyrillic_j
-        )
-        self.cyrillic_j.grid(row=1, column=0, sticky="nsew", padx=(20, 10), pady=10)
-
-        self.deaf_l = customtkinter.CTkButton(
-            self, text="ӆ", command=self.button_deaf_l
-        )
-        self.deaf_l.grid(row=1, column=1, sticky="nsew", padx=10, pady=10)
-
-        self.deaf_m = customtkinter.CTkButton(
-            self, text="ӎ", command=self.button_deaf_m
-        )
-        self.deaf_m.grid(row=1, column=2, sticky="nsew", padx=10, pady=10)
-
-        self.deaf_n = customtkinter.CTkButton(
-            self, text="ӊ", command=self.button_deaf_n
-        )
-        self.deaf_n.grid(row=1, column=3, sticky="nsew", padx=(10, 20), pady=10)
-
-        self.n_with_tail = customtkinter.CTkButton(
-            self, text="ӈ", command=self.button_n_with_tail
-        )
-        self.n_with_tail.grid(row=2, column=0, sticky="nsew", padx=(20, 10), pady=(10, 20))
-
-        self.deaf_r = customtkinter.CTkButton(
-            self, text="ҏ", command=self.button_deaf_r
-        )
-        self.deaf_r.grid(row=2, column=1, sticky="nsew", padx=10, pady=(10, 20))
-
-        self.semi_soft_sign = customtkinter.CTkButton(
-            self, text="ҍ", command=self.button_semi_soft_sign
-        )
-        self.semi_soft_sign.grid(row=2, column=2, sticky="nsew", padx=10, pady=(10, 20))
-
-    def button_dotted_a(self):
-        print("ӓ", end="")
-
-    def button_dotted_e(self):
-        print("ӭ",  end="")
-
-    def button_h(self):
-        print("һ",  end="")
-
-    def button_j(self):
-        print("ј",  end="")
-
-    def button_cyrillic_j(self):
-        print("ҋ", end="")
-
-    def button_deaf_l(self):
-        print("ӆ", end="")
-
-    def button_deaf_m(self):
-        print("ӎ", end="")
-
-    def button_deaf_n(self):
-        print("ӊ", end="")
-
-    def button_n_with_tail(self):
-        print("ӈ", end="")
-
-    def button_deaf_r(self):
-        print("ҏ", end="")
-
-    def button_semi_soft_sign(self):
-        print("ҍ", end="")
-class Search_tools(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
+        ## параметры окна
+        ##self.title("letterpad")
+        ##self.geometry("220x170")
+        self.grid_rowconfigure((0), weight=0)
+        ##self.after(0, lambda: self.state("zoomed"))
+        ##self.minsize(900, 500)
+
+        self.letters = ["ӓ", "ӭ", "һ", "ј", "ҋ", "ӆ", "ӎ", "ӊ", "ӈ", "ҏ", "ҍ", "¯̄"]
+
+        for i, letter in enumerate(self.letters):
+            self.letter_button = customtkinter.CTkButton(
+                self,
+                text=letter[0],
+                command=apply_arguments(self.make_letter_button, letter[-1]),
+                width=35,
+                height=35
+            )
+            self.grid_columnconfigure((i), weight=1)
+            self.letter_button.grid(row=0, column=i, padx=5, pady=5)
+
+    def make_letter_button(self, letter):
+        self.master.entry.insert(len(self.master.entry.get()), letter)
+
+class Search_tools(customtkinter.CTkFrame):
+
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.grid_columnconfigure((0), weight=1)
+        self.grid_rowconfigure((0, 1), weight=0)
+
         self.entry = customtkinter.CTkEntry(self, placeholder_text="введите услышанное")
-        self.entry.grid(row=0, column=1, sticky="nsew", padx=10, pady=(20, 10))
+        self.entry.grid(row=0, column=0, sticky="nsew", padx=(0, 10), pady=(0, 10))
         self.entry.bind("<KeyRelease-Return>", command = self.enter_pressed)
         ##command = self.enter_pressed
+
+        self.win_info = None
+        self.letterpad = None
 
         ## кнопка поиска
         self.button_search = customtkinter.CTkButton(
             self, text="искать!", command = self.master.button_search_event
         )
-        self.button_search.grid(row=0, column=2, sticky="nsew", padx=10, pady=(20, 10))
+        self.button_search.grid(row=0, column=1, sticky="ns", padx=(10, 10), pady=(0, 10))
+
+        self.button_letterpad = customtkinter.CTkButton(
+            self, text="букви!", command=self.call_letterpad
+        )
+        self.button_letterpad.grid(row=0, column=2, sticky="ns", padx=(10, 10), pady=(0, 10))
         ##command = self.button_search_event
 
+        ## кнопка инфо
+        self.button_info = customtkinter.CTkButton(self, text="о проекте")
+        self.button_info.grid(
+            row=0, column=3, sticky="nsew", padx=(10, 10), pady=(0, 10)
+        )
+        self.button_info.bind("<ButtonRelease>", command=self.info)
+
+    def call_letterpad(self):
+        if self.letterpad == None:
+            self.letterpad = Letterpad(self)
+            self.letterpad.grid(row=1, column=0, columnspan=4, sticky="w", padx=0, pady=(0, 0))
+        else:
+            self.letterpad.destroy()
+            self.letterpad = None
 
     def what_enterred(self):
         return self.entry.get()
     def enter_pressed(self, _=None):
         self.master.button_search_event()
+
+    def info(self, _=None):
+        if self.win_info is None or not self.win_info.winfo_exists():
+            self.win_info = WinAbout(self)
+        self.win_info.after(10, self.win_info.focus)
 
 
 class PossibleWords(customtkinter.CTkFrame):
@@ -203,6 +162,8 @@ class App(customtkinter.CTk):
         self.after(0, lambda: self.state("zoomed"))
         self.minsize(900, 500)
 
+
+
         ## варианты
         self.possible_words = PossibleWords(self)
         self.possible_words.grid(
@@ -211,18 +172,12 @@ class App(customtkinter.CTk):
 
         self.search_tools = Search_tools(self)
         self.search_tools.grid(row=0, column=1, sticky="nsew", padx=10, pady=(20, 10))
+        self.search_tools.configure(fg_color="transparent", border_width=0)
 
         ## строка поиска
         ##self.entry.bind("<KeyPress-Return>", command=self.info_waite)
 
         ##self.button_search.bind("<Button>", command=self.info_waite)
-
-        ## кнопка инфо
-        self.button_info = customtkinter.CTkButton(self, text="о проекте")
-        self.button_info.grid(
-            row=0, column=3, sticky="nsew", padx=(10, 20), pady=(20, 10)
-        )
-        self.button_info.bind("<ButtonRelease>", command=self.info)
 
         ##вывод
         self.textbox = cus_ext.CTkPrettyTextbox(self)
@@ -233,10 +188,9 @@ class App(customtkinter.CTk):
             rowspan=2,
             sticky="nesw",
             padx=(10, 20),
-            pady=(10, 20),
+            pady=(0, 20),
         )
 
-        self.win_info = None
         self.letterpad = None
 
     def button_search_event(self):
@@ -248,11 +202,6 @@ class App(customtkinter.CTk):
             ]
         )
         self.after(10, self.find)
-
-    def info(self, _=None):
-        if self.win_info is None or not self.win_info.winfo_exists():
-            self.win_info = WinAbout(self)
-        self.win_info.after(10, self.win_info.focus)
 
     def find(self, _=None):
         res = possible_words_from_input_word(self.search_tools.what_enterred().lower())
