34895869f50eafa69fb6ae848cfdc565dea6e259
Ajno <boyko.artiom@gmail.com>
2024-05-16 12:31:57 +0300

    Подвёл под pep-8

--- utils.py
+++ utils.py
@@ -1,13 +1,19 @@
+"""Функция для создания функции из функции и значений аргументов"""
+
 from typing import Callable
 
-def apply_arguments(func: Callable, *inserted_args, **inserted_kwargs) -> Callable:
-	"""Создаёт функцию, которая работает аналогично функции `func` но с применением аргументов, указанных следом
 
-	:param func: Функция, на основании которой нужно создать новую функцию
-	:type func: Callable
-	:return: Функция, работающая аналогично старой, но с применением введённых аргументов
-	:rtype: Callable
-	"""
-	def new_func(*args, **kwargs):
-		func(*inserted_args, *args, **inserted_kwargs, **kwargs)
-	return new_func
+def apply_arguments(func: Callable, *inserted_args, **inserted_kwargs) -> Callable:
+    """Создаёт функцию, которая работает аналогично функции `func`,
+    но с применением аргументов, указанных следом
+
+    :param func: Функция, на основании которой нужно создать новую функцию
+    :type func: Callable
+    :return: Функция, работающая аналогично старой, но с применением введённых аргументов
+    :rtype: Callable
+    """
+
+    def new_func(*args, **kwargs):
+        func(*inserted_args, *args, **inserted_kwargs, **kwargs)
+
+    return new_func
